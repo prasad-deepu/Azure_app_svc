7c98be54b1b394c6d51ee686fe659efdfcfd6ed7
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2022-10-02 13:37:28 +0000

    changes

--- variables.tf
+++ variables.tf
@@ -2,3 +2,5 @@ variable "resource_group_name" {
   type    = string
   description = "The RG name of Azure"
 }
+
+#terraform import azurerm_resource_group.rg /subscriptions/548c659d-d1cf-4217-8082-70230840e278/resourceGroups/learn-d35d078c-1f65-4cd0-94dc-4931aa4153ee
