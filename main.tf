c7e8b130bddce45e1b87c03ae3e420e9585bd8a1
prasad-deepu <58420480+prasad-deepu@users.noreply.github.com>
2022-10-04 09:52:17 +0000

    added main file tht is deployed to azure app

--- main.tf
+++ main.tf
@@ -21,22 +21,22 @@ resource "random_integer" "ri" {
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = "westus"
+  location = "eastus"
 }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
+  name                = "ASP-DevSecOpsSharedLibrary-a05b"
+  location            = "centralus"
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "P3v2"
+  sku_name            = "P1v2"
 }
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
   name                  = "webapp-${random_integer.ri.result}"
-  location              = azurerm_resource_group.rg.location
+  location              = azurerm_service_plan.appserviceplan.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   https_only            = true
